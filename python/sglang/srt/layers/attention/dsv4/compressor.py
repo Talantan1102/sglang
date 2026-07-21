2a65a5231bbc27ce167fe2d3f490705a3e9a454b
root <root@localhost.localdomain>
2026-07-21 07:51:23 +0000

    Revert "dsv4 CP v1"
    This reverts commit 209705ea92b24d1381914459d4054d35f5d3c7bd.

--- python/sglang/srt/layers/attention/dsv4/compressor.py
+++ python/sglang/srt/layers/attention/dsv4/compressor.py
@@ -470,12 +470,7 @@ class Compressor(MultiPlatformOp):
             assert x.shape[0] == 0
             return x.new_empty(0, self.head_dim)
 
-        use_cp = (
-            getattr(forward_batch, "attn_cp_metadata", None) is not None
-            and forward_batch.forward_mode.is_context_parallel_extend()
-            and get_attention_cp_size() > 1
-        )
-        if dsa_use_prefill_cp(forward_batch) or use_cp:
+        if dsa_use_prefill_cp(forward_batch):
             x = cp_all_gather_rerange_output(
                 x,
                 get_attention_cp_size(),
